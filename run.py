48917d38be9f3a1c3122538df5017c6f8468b077
Squirkey89 <squirke89@gmail.com>
2022-02-05 13:43:14 +0000

    Added a main game function also added the first quiz question

--- run.py
+++ run.py
@@ -25,44 +25,65 @@ def start_screen():
     Then they are prompted to press the enter key
     """
 
-
     print(f"""
   /$$$$$$                                  /$$
  /$$__  $$                                | $$
 | $$  \__/  /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$   /$$$$$$$
-|  $$$$$$  /$$__  $$ /$$__  $$ /$$__  $$|_  $$_/  /$$_____/ 
- \____  $$| $$  \ $$| $$  \ $$| $$  \__/  | $$   |  $$$$$$    
+|  $$$$$$  /$$__  $$ /$$__  $$ /$$__  $$|_  $$_/  /$$_____/
+ \____  $$| $$  \ $$| $$  \ $$| $$  \__/  | $$   |  $$$$$$
  /$$  \ $$| $$  | $$| $$  | $$| $$        | $$ /$$\____  $$
-|  $$$$$$/| $$$$$$$/|  $$$$$$/| $$        |  $$$$//$$$$$$$/   
- \______/ | $$____/  \______/ |__/         \___/ |_______/  
-          | $$                                                       
-          | $$                                                                                     
-          |__/ 
-  /$$$$$$            /$$          
- /$$__  $$          |__/             
-| $$  \ $$ /$$   /$$ /$$ /$$$$$$$$          
-| $$  | $$| $$  | $$| $$|____ /$$/           
-| $$  | $$| $$  | $$| $$   /$$$$/            
-| $$/$$ $$| $$  | $$| $$  /$$__/           
-|  $$$$$$/|  $$$$$$/| $$ /$$$$$$$$ 
-\____ $$$ \______/ |__/|________/    
+|  $$$$$$/| $$$$$$$/|  $$$$$$/| $$        |  $$$$//$$$$$$$/
+ \______/ | $$____/  \______/ |__/         \___/ |_______/
+          | $$
+          | $$
+          |__/
+  /$$$$$$            /$$
+ /$$__  $$          |__/
+| $$  \ $$ /$$   /$$ /$$ /$$$$$$$$
+| $$  | $$| $$  | $$| $$|____ /$$/
+| $$  | $$| $$  | $$| $$   /$$$$/
+| $$/$$ $$| $$  | $$| $$  /$$__/
+|  $$$$$$/|  $$$$$$/| $$ /$$$$$$$$
+\____ $$$ \______/ |__/|________/
 """)
 
     key = input("Press the enter key to go to the main menu\n")
 
 start_screen()
 
+def main_game():
+    """
+    This is the Quiz for the user to play it is multiple choice
+    and has four options for the user to choose from.
+    """
+    print("Let the quiz begin\n")
+
+    question_answer = input("""Which country is hosting the 2022 FIFA World Cup?
+    A: Quatar 
+    B: Uganda
+    C: Vietnam
+    D: Bolivia\n """)
+    if question_answer == 'A' or question_answer == 'a':
+        print('Correct well done!')
+    elif question_answer == 'B'or 'C' or 'D'or question_answer == 'b' or 'c' or 'd':
+        print('Unlucky incorrect answer')
+    else:
+        print('Invalid choice please choose a, b, c or d')
+    
+
+
+
 def main_menu():
     """
-    Upon passing the start screen. The user will be presented 
+    Upon passing the start screen. The user will be presented
     with three options. First to play the game.
     The second option is Instructions for playing the game.
-    The final option for the user is to exit the game.   
+    The final option for the user is to exit the game.
     """
     print("Main Menu\n")
 
-    choice = input(""" A: Start Game\n B: Instructions\n C: Exit\n
-    Please enter your choice here:\n """)
+    print(""" A: Start Game\n B: Instructions\n C: Exit\n """)
+    choice = input ("Please enter your choice here:\n ")
 
     if choice == "a" or choice == "A":
         main_game()
@@ -71,18 +92,9 @@ def main_menu():
     elif choice == "c" or choice == "C":
         exit()
     else:
-        print("Your choice is incorrect please pick a valide choice")
+        print("Your choice is incorrect please pick a valid choice")
 
-
-    
 main_menu()
+    
+main_game()
 
-def exit():
-    """
-    This is when the user can exit the main menu
-    The user will return to the start screen
-    """
-    start_screen()
-
-exit()
- 
