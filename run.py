7f173bc4cb13d6f8b7aa6decadce1932ed6ac6d1
Squirkey89 <squirke89@gmail.com>
2022-02-08 18:29:25 +0000

    Added a new function called instructions which user can access from the main menu

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from questions import questions
+import questions
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -55,6 +55,35 @@ def start_screen():
 start_screen()
 start = input("Press enter to got to the main menu\n")
 
+def instructions():
+    """
+    The user will hear be introduced to the insyructions on how to play the quiz.
+    """
+    print("")
+    print("****************************Instructions*************************************")
+    print("")
+    print("You will be tested on your sporting knowledge with 11 sport quiz questions.\n")  
+    print("The questions are multiple choice, so you will have to select a, b, c, or d.\n")
+    print("Upon making your choice, you will be notified of how you performed on this question.\n")
+    print("When the question is answered, the next question will appear.\n")
+    print("You will receive your quiz results at the end. Best of luck!!!\n")
+    print("")
+    print("Press esc to return to the menu screen or press p to start the game.")
+    print("")
+
+instructions()
+
+
+
+
+def main_game():
+    """
+    This is the Quiz for the user to play it is multiple choice
+    and has four options for the user to choose from.
+    """
+
+main_game()
+
 def main_menu():
     """
     Upon passing the start screen. The user will be presented
@@ -62,8 +91,8 @@ def main_menu():
     The second option is Instructions for playing the game.
     The final option for the user is to exit the game.
     """
-    
-    print("Main Menu\n")
+
+    print("****************************Main Menu*************************************\n")
 
     print(""" A: Start Game\n B: Instructions\n C: Exit\n """)
     choice = input("Please enter your choice here:\n")
@@ -78,22 +107,4 @@ def main_menu():
         print("Your choice is incorrect please choose a valid option\n")
         input("Choose one of a,b,c:")
 
-
-def main_game(questions):
-    """
-    This is the Quiz for the user to play it is multiple choice
-    and has four options for the user to choose from.
-    """
-
 main_menu()
-
-
-play = True
-while play:
-    result = 0
-    questions = 11
-
-    print("Let the quiz begin\n")
-
-
-main_game()
