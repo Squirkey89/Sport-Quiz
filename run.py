5927e041094d3dc57674343881441f89b4e8a0ba
Squirkey89 <squirke89@gmail.com>
2022-02-16 04:49:59 +0000

    Update code with pep8 validation

--- run.py
+++ run.py
@@ -78,10 +78,10 @@ def quiz_questions():
 
 def start_screen():
     """
-    This is the beginning of the quiz for 
-    the user they will first find an 
-    introduction to the game with the quiz 
-    name and images. Then they are 
+    This is the beginning of the quiz for
+    the user they will first find an
+    introduction to the game with the quiz
+    name and images. Then they are
     prompted to press the enter key
     """
 
